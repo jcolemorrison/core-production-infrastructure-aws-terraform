f0aa12b68d0f9e9390c03fbec67e71b3b5303337
J Cole Morrison <me@jcolemorrison.com>
2022-11-26 14:55:11 -0800

    add VPC subnets and update image

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Everything is written using Infrastructure as Code via Terraform so that you can
 
 ## The Architecture Analogy
 
-![The Servers and Compute to City Analogy](images/core-building-blocks-2.png)
+![The Servers and Compute to City Analogy](images/core-building-blocks-3.png)
 
 The Different States in Descending Order:
 
--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,36 @@ resource "aws_vpc" "main" {
 
   tags = { "Name" = "${local.project_tag}-vpc" }
 }
+
+# Subnets - Districts
+
+## Public Subnets
+resource "aws_subnet" "public" {
+  count = var.vpc_public_subnet_count
+
+  vpc_id = aws_vpc.main.id
+
+  # create subnets based on the vpc's cidr_block and chosen count
+  cidr_block              = local.public_cidr_blocks[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  # create ipv6 subnets based on the vpc's cidr_block and chosen count
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
+  assign_ipv6_address_on_creation = true
+
+  tags = { "Name" = "${local.project_tag}-public-${data.aws_availability_zones.available.names[count.index]}" }
+}
+
+## Private Subnets
+resource "aws_subnet" "private" {
+  count = var.vpc_private_subnet_count
+
+  vpc_id = aws_vpc.main.id
+
+  // Increment the netnum by the number of public subnets to avoid overlap
+  cidr_block = local.private_cidr_blocks[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = { "Name" = "${local.project_tag}-private-${data.aws_availability_zones.available.names[count.index]}" }
+}
